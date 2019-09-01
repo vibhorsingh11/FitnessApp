43a52a5b62e302e873bc9024824b8fa127d9a024
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-01 16:00:21 +0530

    App structure setup

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import {MatButtonModule, MatIconModule} from '@angular/material';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatIconModule],
 })
 export class MaterialModule {}
