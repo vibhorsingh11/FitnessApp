5b6ee4abd255cdb92192a5b46c8c0e345e97527d
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-05 15:48:21 +0530

    Added sidenav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule,
-} from '@angular/material';
+MatSidenavModule, MatToolbarModule} from '@angular/material';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-      MatDatepickerModule, MatNativeDateModule],
+      MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule],
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule],
+    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule],
 })
 export class MaterialModule {}
