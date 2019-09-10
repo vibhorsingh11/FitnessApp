735b654ea8f0f1e8a08524c2ed432b214c185f27
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-10 21:45:14 +0530

    Added chnages in training section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { PastTrainingComponent } from './training/past-training/past-training.co
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { StopTrainingComponent } from './training/current-training/stop-training.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     PastTrainingComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent
+    SidenavListComponent,
+    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
@@ -40,6 +42,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [StopTrainingComponent]
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule,
-MatSidenavModule, MatToolbarModule, MatListModule} from '@angular/material';
+MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
+MatProgressSpinnerModule, MatDialogModule} from '@angular/material';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-      MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule],
+      MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
+      MatSelectModule, MatProgressSpinnerModule, MatDialogModule],
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule],
+    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
+    MatSelectModule, MatProgressSpinnerModule, MatDialogModule],
 })
 export class MaterialModule {}
