7125ce9018e93b2a184c983127fab9424fd5d9e0
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-12 18:38:45 +0530

    updated with router navigation

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
@@ -7,12 +8,14 @@ export class AuthService {
     authChange = new Subject<boolean>();
     private user: User;
 
+    constructor(private router: Router) {}
+
     registerUser(authData: AuthData) {
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     login(authData: AuthData) {
@@ -20,12 +23,13 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     logout() {
         this.user = null;
         this.authChange.next(false);
+        this.router.navigate(['/login']);
     }
 
     getUser() {
@@ -34,5 +38,10 @@ export class AuthService {
 
     isAuth() {
         return this.user != null;
+    }
+
+    private authSuccessfully() {
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
     }
 }
