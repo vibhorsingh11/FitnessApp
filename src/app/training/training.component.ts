5aa259e6f2b9b8e7df21b7d6b42ddda5b219cc8b
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-14 23:17:27 +0530

    Updated the training section

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+import { TrainingService } from './training.service';
+
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
@@ -7,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
   ongoingTraining = false;
+  exerciseSubscription: Subscription;
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(exercise => {
+      if (exercise) {
+        this.ongoingTraining = true;
+      } else {
+        this.ongoingTraining = false;
+      }
+    });
   }
 
 }
