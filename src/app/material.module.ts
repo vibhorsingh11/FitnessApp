f2ea52add9eabf226ba2ce14140f81661affcd64
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-15 22:06:36 +0530

    added sorting & filtering to datatable

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule,
 MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
-MatProgressSpinnerModule, MatDialogModule, MatTableModule} from '@angular/material';
+MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule} from '@angular/material';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
       MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
-      MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule],
+      MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule],
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
     MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
-    MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule],
+    MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule],
 })
 export class MaterialModule {}
