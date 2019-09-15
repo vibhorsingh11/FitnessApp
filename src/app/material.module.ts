69794e6bf25fbd1c1a2c8055ced653c86cd1b6ab
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-15 22:17:13 +0530

    added pagination to datatable

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule,
 MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
-MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule} from '@angular/material';
+MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule} from '@angular/material';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
       MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
-      MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule],
+      MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule],
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
     MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule,
-    MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule],
+    MatSelectModule, MatProgressSpinnerModule, MatDialogModule, MatTableModule, MatSortModule, MatPaginatorModule],
 })
 export class MaterialModule {}
