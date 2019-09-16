833ae518798e40965cac172c1a8ee255ad520fc2
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-16 13:46:46 +0530

    setup angularFire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDnV1C4i6_SPCUMETiFT1ThWAlcPEYAhY4',
+    authDomain: 'ng-fitness-tracker-4f9e3.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-4f9e3.firebaseio.com',
+    projectId: 'ng-fitness-tracker-4f9e3',
+    storageBucket: 'ng-fitness-tracker-4f9e3.appspot.com',
+    messagingSenderId: '599096547347',
+    appId: '1:599096547347:web:54483671ddfe03783787e6'
+  }
 };
 
 /*
