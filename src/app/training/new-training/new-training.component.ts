10d729784db5d1c42596a06922cf75f59341a36f
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-17 13:47:05 +0530

    connected to fetch excercises with firebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -10,12 +13,23 @@ import { TrainingService } from '../training.service';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Exercise[];
+  exercises: Observable<Exercise[]>;
 
-  constructor(private trainingService: TrainingService) { }
+  constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.exercises = this.trainingService.getAvailableExercises();
+    this.exercises = this.db.collection('availableExercises').snapshotChanges().pipe(map(docArray => {
+                        return docArray.map(doc => {
+                          console.log(doc);
+                          return {
+                            id: doc.payload.doc.id,
+                            // name: doc.payload.doc.data().name,
+                            // duration: doc.payload.doc.data().duration,
+                            // calories: doc.payload.doc.data().calories,
+                            ...doc.payload.doc.data()
+                          }as Exercise;
+                        });
+                      }));
   }
 
   onStartTraining(form: NgForm) {
