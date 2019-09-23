8cc6e6c4a277269d95e9a2e03cbd4ebd07e61856
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-23 14:07:11 +0530

    updated gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDnV1C4i6_SPCUMETiFT1ThWAlcPEYAhY4',
+    authDomain: 'ng-fitness-tracker-4f9e3.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-4f9e3.firebaseio.com',
+    projectId: 'ng-fitness-tracker-4f9e3',
+    storageBucket: 'ng-fitness-tracker-4f9e3.appspot.com',
+    messagingSenderId: '599096547347',
+    appId: '1:599096547347:web:54483671ddfe03783787e6'
+  }
 };
