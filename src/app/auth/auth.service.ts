cf317be47b661f597e15fcc8e78ee31a7ae61d77
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-09-27 22:33:44 +0530

    added auth reducer

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Store } from '@ngrx/store';
@@ -9,11 +8,10 @@ import { TrainingService } from '../training/training.service';
 import { UIService } from '../shared/ui.service';
 import * as fromRoot from '../app.reducer';
 import * as UI from '../shared/ui.actions';
+import * as Auth from '../auth/auth.actions';
 
 @Injectable()
 export class AuthService {
-    authChange = new Subject<boolean>();
-    private isAuthenticated = false;
 
     constructor(private router: Router, private afuth: AngularFireAuth, private trainingService: TrainingService,
                 private uiService: UIService, private store: Store<{ui: fromRoot.State}>) {}
@@ -21,14 +19,12 @@ export class AuthService {
     initAuthListener() {
         this.afuth.authState.subscribe(user => {
             if (user) {
-                this.isAuthenticated = true;
-                this.authChange.next(true);
+                this.store.dispatch(new Auth.SetAuthenticated());
                 this.router.navigate(['/training']);
             } else {
                 this.trainingService.cancelSubscriptions();
-                this.authChange.next(false);
+                this.store.dispatch(new Auth.SetUnAuthenticated());
                 this.router.navigate(['/login']);
-                this.isAuthenticated = false;
             }
         });
     }
@@ -65,9 +61,5 @@ export class AuthService {
 
     logout() {
         this.afuth.auth.signOut();
-    }
-
-    isAuth() {
-        return this.isAuthenticated;
     }
 }
